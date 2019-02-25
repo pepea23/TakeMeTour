e887207febe27147403a1f9bbbc3df59afbc0423
pepea <pepea.23@live.com>
2019-02-25 23:06:31 +0700

    [ADD] Function and state call joke

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,10 +1,43 @@
 import React, { Fragment, Component } from 'react'
-
+import Joke from './joke'
+import jokeService from './../../service/JokeService'
 class index extends Component {
+
+  state = {
+    user:{
+      firstName:'',
+      lastName:'',
+      id:0
+    },
+    jokes:{
+      categories:[''],
+      id:0,
+      joke:'Hahah!'
+    }
+  }
+  handleField =(name,value)=>{
+    let userDb = this.state.user
+    this.setState({
+  user:{
+    ...userDb,
+    [name]:value
+  }
+})
+  }
+  handleJoke=async (method)=>{
+   let res = await jokeService.getJokebyId(this.state.user.id,this.state.user)
+    this.setState({
+      jokes:res.data.value
+    })
+  }
   render () {
     return (
       <Fragment>
-
+        <Joke 
+        user ={this.state.user}
+        jokes= {this.state.jokes}
+         handleField={this.handleField}
+          handleJoke={this.handleJoke}/>
       </Fragment>
     )
   }
--- components/Joke/joke.js
+++ components/Joke/joke.js
@@ -1,10 +1,91 @@
 import React, { Component } from 'react'
+import {
+  Card,
+  Form,
+  Input,
+  Button,
+  Select as AntDesignSelect
+} from 'antd'
+const FormItem = Form.Item
 
 class joke extends Component {
   render () {
     return (
-      <div>
+      <div className=" container-fluid">
+        <div className="row">
+          <div className="col-4"></div>
+          <div className="col-4">
+            <Card className='mt-5'>
+              <form>
+                <div className=" container-fluid">
+                  <div className="row">
+                    <div className="col-6">
+                      <FormItem>
+                FirstName :
+                      </FormItem>
+                    </div>
+                    <div className="col-6">
+                      <FormItem>
+                        <Input
+                          onChange={({ target: { name, value } }) =>
+                            this.props.handleField(name, value)
+                          }
+                          name="firstName"
+                          value={this.props.user.firstName}
+                        />
+                      </FormItem>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-6">
+                      <FormItem>
+                LastName :
+                      </FormItem>
+                    </div>
+                    <div className="col-6">
+                      <FormItem>
+                        <Input
+                          onChange={({ target: { name, value } }) =>
+                            this.props.handleField(name, value)
+                          }
+                          name="lastName"
+                          value={this.props.user.lastName}
+                        />
+                      </FormItem>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-6">
+                      <FormItem>
+                JokeId :
+                      </FormItem>
+                    </div>
+                    <div className="col-6">
+                      <FormItem>
+                        <Input
+                          type="number"
+                          onChange={({ target: { name, value } }) =>
+                            this.props.handleField(name, value)
+                          }
+                          name="id"
+                          value={this.props.user.id}
+                        />
+                      </FormItem>
+                    </div>
+                  </div>
+                </div>
 
+              </form>
+              <div className="row">
+                <Button onClick={() => this.props.handleJoke()}>Joke!</Button>
+              </div>
+              <div className='row'>
+                {this.props.jokes.joke}
+              </div>
+            </Card>
+          </div>
+          <div className="col-4"></div>
+        </div>
       </div>
     )
   }
