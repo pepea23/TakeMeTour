0758458457cd3e71c93a7d2cf40ad4263173b19c
pepea <pepea.23@live.com>
2019-02-25 21:08:40 +0700

    [ADD] setup joke index

---
+++ components/Joke/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment, Component } from 'react'
+
+class index extends Component {
+  render () {
+    return (
+      <Fragment>
+
+      </Fragment>
+    )
+  }
+}
+
+export default index
---
+++ pages/joke.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import App from './../components/Joke'
+class joke extends Component {
+  render () {
+    return (
+      <App/>
+    )
+  }
+}
+
+export default joke
