699c8663e820402f561f1f3f6ac6aa2820b6e203
pepea <pepea.23@live.com>
2019-02-26 14:39:56 +0700

    [ADD] get alot randoms jokes

--- service/JokeService.js
+++ service/JokeService.js
@@ -1,12 +1,17 @@
 import api from './../utils/ApiTest'
+import { async } from 'rxjs/internal/scheduler/async'
 
 const jokes = {
   getJokebyId: async (id, user) => {
-    let res = await api.get(`${id}?firstName=${user.firstName}&lastName=${user.firstName}`)
+    let res = await api.get(`${id}?firstName=${user.firstName}&lastName=${user.lastName}`)
     return res
   },
   getRandomJoke: async (user) => {
-    let res = await api.get(`random?firstName=${user.firstName}&lastName=${user.firstName}`)
+    let res = await api.get(`random?firstName=${user.firstName}&lastName=${user.lastName}`)
+    return res
+  },
+  getRandomJokes: async (num, user) => {
+    let res = await api.get(`random/${num}?firstName=${user.firstName}&lastName=${user.lastName}`)
     return res
   }
 
